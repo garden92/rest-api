6a8769941e01c4ba74d7494c380182f083d5985d
백철우 (KT협력사) <91347427@ktpartners.com>
2025-05-26 18:29:25 +0900

    :sparkles: add globalhandler

--- app/src/main/java/com/kt/kol/app/handler/GlobalExceptionHandler.java
+++ app/src/main/java/com/kt/kol/app/handler/GlobalExceptionHandler.java
@@ -6,16 +6,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kt.kol.common.exception.BusinessException;
 import com.kt.kol.common.model.ResponseStdVO;
+import com.kt.kol.common.model.TrtErrInfoDTO;
+import com.kt.kol.common.util.StringUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
+@RequiredArgsConstructor
+@Slf4j
 public class GlobalExceptionHandler {
 
-	// TODO : TrtBaseInfoDTO 제거
 	@ExceptionHandler(value = BusinessException.class)
 	@ResponseBody
 	public ResponseStdVO<Void> handleBizException(
 			BusinessException e) {
 		return ResponseStdVO.businessError(e.getTrtErrInfoDTO());
 	}
-
+	
+	// RuntimeException 처리
+	@ExceptionHandler(value = Exception.class)
+	@ResponseBody
+	public ResponseStdVO<Void> handleException(Exception e) {
+		log.error(StringUtil.printExceptionStack(e));
+		return ResponseStdVO.systemError(new TrtErrInfoDTO("S", "KOLS0001"
+							, e.getMessage(), StringUtil.printExceptionStack(e)));
+	}
 }
