09b851adb925fd8be3507f0ad938354bbbf1ec5e
jungwon-seo_ktdev <jungwon.seo@kt.com>
2025-07-22 14:25:56 +0900

    :fire: 불필요한 초기 설정 삭제

--- app/src/main/java/com/kt/kol/app/Application.java
+++ app/src/main/java/com/kt/kol/app/Application.java
@@ -2,15 +2,11 @@ package com.kt.kol.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = { "com.kt.kol" })
 @ComponentScan(basePackages = { "com.kt.kol" })
-@EnableScheduling
 @EnableR2dbcRepositories(basePackages = "com.kt.kol.api.**.repository")
 public class Application {
 
